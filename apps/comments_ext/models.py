8ca6d08a8b02975502b412759f53b29aeef51147
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2013-06-10 06:10:32 +0200

    comments in admin

--- apps/comments_ext/models.py
+++ apps/comments_ext/models.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.comments.models import Comment
 
 
 class CommentWithPrivate(Comment):
-    is_private = models.BooleanField(_('is private'), default=False)
+    is_private = models.BooleanField(u'prywatny', default=False,
+        help_text=u'zaznacz to pole aby komentarz był widoczny tylko dla zalogowanych użytkowników')
+
+    class Meta:
+        verbose_name = _('comment')
+        verbose_name_plural = _('comments')
